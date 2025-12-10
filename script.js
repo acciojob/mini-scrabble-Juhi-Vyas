c54385b40770756dd38f6fedc3108f27e1fbf545
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-10 16:56:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your code here
+let text = document.getElementById("evaluatedText")
+let letter = document.getElementById("letterCount")
+
+let count = 1
+text.addEventListener("keydown", (event)=>{
+	if(event.key == "Backspace"){
+		letter.innerHTML = count--
+	}else{
+		letter.innerHTML = count++
+	}
+
+		// letter.innerHTML = event.key
+})
