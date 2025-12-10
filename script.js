f1e358d8946fc30522ab03d9c568f0afadb60276
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-10 17:15:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ let text = document.getElementById("evaluatedText")
 let letter = document.getElementById("letterCount")
 
 let count = 0
-text.addEventListener("keydown", (event)=>{
-	if(event.key == "Backspace"){
-		if(count > 0){
-			count--
-		    letter.innerHTML = count
-		}
-	}else{
-		count++
-		letter.innerHTML = count
-	}
-
-		// letter.innerHTML = event.key
+text.addEventListener("input", (event)=>{
+	letter.innerHTML = text.value.length
+	// if(event.key == "Backspace"){
+	// 	if(count > 0){
+	// 		count--
+	// 	    letter.innerHTML = count
+	// 	}
+	// }else{
+	// 	count++
+	// 	letter.innerHTML = count
+	// }
 })
